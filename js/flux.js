2eb3cc46f7e1a8023bc6311fb2f90333114f6364
Luke Garwood <luke_gar@hotmail.com>
2015-08-01 00:36:18 -0400

    added mask screen during audio play, innerHTML changes, and date conditions

--- js/flux.js
+++ js/flux.js
@@ -1,26 +1,64 @@
 
-
+//this boolean may no longer be neccessary with the block out screen coming up
 var tf = false;
-var audio = new Audio("media/powwow.mp3");
+
+
+var canvas = document.getElementById('playerLayer');
+var date = new Date();
+var day = date.getDate();
+var audio = document.createElement('audio');
+audio.src = 'media/powwow.mp3';
+
+
+
 
 
 var android = function(){
-  var location = confirm('If at a FluxDelux location press OK');
-  if(location === true && tf === false){
-    audio.play();
-    tf = true;
+  var location = confirm("Flux Delux gets going at Nathan Philips Square, on August 8th, 2015! If you're already there, get your head phones plugged in and ready to go, then hit OK");
+    if(location === true && day === 31 && tf === false){
+        audio.play();
+        canvas.style.visibility = "visible";
+        tf===true
 
-  } else{
-    console.log('Bye');
+    } else if(location === true && day !== 31){
+        document.getElementById("b1").textContent = "Come back on August 8th!";
+  };
+  audio.onended=function(){
+    tf=false;
+    canvas.style.visibility ="hidden";
+    document.getElementById("b1").textContent = "Thank you for participating!";
+    console.log("done");
   }
-//---adding controls to the streaming?
-  $("body").dblclick(function(){
-    audio.pause();
-  });
-
-
 };
 
-var appDirect = function(){
-  window.open("https://geo.itunes.apple.com/us/app/fluxdelux/id997524033?mt=8")
-}
+
+
+// var android = function(){
+//   if(press === true && tf === false){
+//     audio.play();
+//     tf === true;
+//   } else{
+//     buttonChange();
+//   };
+//
+//   audio.onended=function(){
+//     tf=false;
+//
+//     document.getElementById("b1").textContent = "Thank you for participating!";
+//     console.log("done");
+//   }
+// };
+
+
+
+// ---adding controls to the streaming?
+//   $("body").dblclick(function(){
+//     audio.pause();
+//   });
+//
+//
+// };
+
+// var appDirect = function(){
+//   window.open("https://itunes.apple.com/ca/app/fluxdelux/id997524033?mt=8&ign-mpt=uo%3D4");
+// };
