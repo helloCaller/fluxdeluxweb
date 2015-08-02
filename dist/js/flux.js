4eee4643767b94500172e7c3c0a12d1860a359f4
Luke Garwood <luke_gar@hotmail.com>
2015-08-02 12:06:49 -0400

    fixed logo, changed video to an embed, added bottom image/border

--- dist/js/flux.js
+++ dist/js/flux.js
@@ -9,25 +9,30 @@ var day = date.getDate();
 var audio = document.createElement('audio');
 audio.src = 'media/powwow.mp3';
 
-
+var pauseCheck = function(){
+  if(audio.stop()===true){
+    console.log('pause')
+  }
+};
+pauseCheck();
 
 
 
 var android = function(){
   var location = confirm("Flux Delux gets going at Nathan Philips Square, on August 8th, 2015! If you're already there, get your head phones plugged in and ready to go, then hit OK");
-    if(location === true && day === 31 && tf === false){
+    if(location === true && day === 2 && tf === false){
         audio.play();
         canvas.style.visibility = "visible";
         tf===true
 
-    } else if(location === true && day !== 31){
-        document.getElementById("b1").textContent = "Come back on August 8th!";
+    } else if(location === true && day !== 2){
+        document.getElementById("b1").innerHTML = '<img src = "img/android.png" width=\'25px\' height=\'25px\'>' + " " + "Come back on August 8th!";
   };
   audio.onended=function(){
     tf=false;
     canvas.style.visibility ="hidden";
-    document.getElementById("b1").textContent = "Thank you for participating!";
-    console.log("done");
+    document.getElementById("b1").innerHTML = "Thank you for participating!";
+
   }
 };
 
