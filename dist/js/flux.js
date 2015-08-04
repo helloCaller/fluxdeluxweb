d5eec9e6c933459af661a64284067bd34470c8d1
Luke Garwood <luke_gar@hotmail.com>
2015-08-03 23:04:30 -0400

    ready for android testing

--- dist/js/flux.js
+++ dist/js/flux.js
@@ -1,31 +1,28 @@
 
-//this boolean may no longer be neccessary with the block out screen coming up
+
 var tf = false;
 
 
 var canvas = document.getElementById('playerLayer');
+
 var date = new Date();
 var day = date.getDate();
+
 var audio = document.createElement('audio');
 audio.src = 'media/powwow.mp3';
 
-var pauseCheck = function(){
-  if(audio.stop()===true){
-    console.log('pause')
-  }
-};
-pauseCheck();
+var currentDay = 3;
 
 
 
 var android = function(){
   var location = confirm("Flux Delux gets going at Nathan Philips Square, on August 8th, 2015! If you're already there, get your head phones plugged in and ready to go, then hit OK");
-    if(location === true && day === 2 && tf === false){
+    if(location === true && day === currentDay && tf === false){
         audio.play();
         canvas.style.visibility = "visible";
         tf===true
 
-    } else if(location === true && day !== 2){
+    } else if(location === true && day !== currentDay){
         document.getElementById("b1").innerHTML = '<img src = "img/android.png" width=\'25px\' height=\'25px\'>' + " " + "Come back on August 8th!";
   };
   audio.onended=function(){
@@ -35,35 +32,3 @@ var android = function(){
 
   }
 };
-
-
-
-// var android = function(){
-//   if(press === true && tf === false){
-//     audio.play();
-//     tf === true;
-//   } else{
-//     buttonChange();
-//   };
-//
-//   audio.onended=function(){
-//     tf=false;
-//
-//     document.getElementById("b1").textContent = "Thank you for participating!";
-//     console.log("done");
-//   }
-// };
-
-
-
-// ---adding controls to the streaming?
-//   $("body").dblclick(function(){
-//     audio.pause();
-//   });
-//
-//
-// };
-
-// var appDirect = function(){
-//   window.open("https://itunes.apple.com/ca/app/fluxdelux/id997524033?mt=8&ign-mpt=uo%3D4");
-// };
