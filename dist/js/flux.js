aadc9d63fe8702f1ab55f2f5d74830c59af1c062
Luke Garwood <luke_gar@hotmail.com>
2015-09-15 13:31:06 -0400

    added CORS code

--- dist/js/flux.js
+++ dist/js/flux.js
@@ -18,6 +18,68 @@ var fluxDay = 8;
 
 
 //---SSE
+
+// function createCORSRequest(method, url){
+//     var xhr = new XMLHttpRequest();
+//     if ("withCredentials" in xhr){
+//         // XHR has 'withCredentials' property only if it supports CORS
+//         xhr.open(method, url, true);
+//     } else if (typeof XDomainRequest != "undefined"){ // if IE use XDR
+//         xhr = new XDomainRequest();
+//         xhr.open(method, url);
+//     } else {
+//         xhr = null;
+//     }
+//     return xhr;
+// }
+// Create the XHR object.
+function createCORSRequest(method, url) {
+  var xhr = new XMLHttpRequest();
+  if ("withCredentials" in xhr) {
+    // XHR for Chrome/Firefox/Opera/Safari.
+    xhr.open(method, url, true);
+  } else if (typeof XDomainRequest != "undefined") {
+    // XDomainRequest for IE.
+    xhr = new XDomainRequest();
+    xhr.open(method, url);
+  } else {
+    // CORS not supported.
+    xhr = null;
+  }
+  return xhr;
+}
+
+// Helper method to parse the title tag from the response.
+function getTitle(text) {
+  return text.match('<title>(.*)?</title>')[1];
+}
+
+// Make the actual CORS request.
+function makeCorsRequest() {
+  // All HTML5 Rocks properties support CORS.
+  var url = 'http://updates.html5rocks.com';
+
+  var xhr = createCORSRequest('GET', url);
+  if (!xhr) {
+    alert('CORS not supported');
+    return;
+  }
+
+  // Response handlers.
+  xhr.onload = function() {
+    var text = xhr.responseText;
+    var title = getTitle(text);
+    alert('Response from CORS request to ' + url + ': ' + title);
+  };
+
+  xhr.onerror = function() {
+    alert('Woops, there was an error making the request.');
+  };
+
+  xhr.send();
+}
+
+
 if (!!window.EventSource){
 var source = new EventSource("http://cohort-server-dev.herokuapp.com/broadcast");
 console.log("hello");
@@ -25,7 +87,7 @@ console.log("hello");
 } else {
   console.log("nope");
 }
- source.addEventListener('Episode 1 Go', function(e){
+ source.addEventListener('Episode-1-Go', function(e){
    if (e.origin != 'http://cohort-server-dev.herokuapp.com/broadcast') {
     alert('Origin was not http://example.com');
     console.log("hello episode 1");
